cc41210206fb0b0e5c98aa33e2c8ea5fd10a0dc6
Jon-1103 <jonathan.martins3@fatec.sp.gov.br>
2024-10-24 10:44:34 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Wait for the document to be ready
 document.addEventListener('DOMContentLoaded', function () {
   // Create a map instance and set the initial view coordinates and zoom level
-  var map = L.map('map').setView([51.505, -0.09], 13);
+  var map = L.map('map').setView([-23.516858811278148, -46.83549726328389], 13);
 
 
   // Add a tile layer to the map from OpenStreetMap
@@ -11,6 +11,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
   // Create a marker with popup and add it to the map
-  var marker = L.marker([51.505, -0.09]).addTo(map);
+  var marker = L.marker([-23.516858811278148, -46.83549726328389]).addTo(map);
   marker.bindPopup("Hello, I'm a marker!").openPopup();
 });
