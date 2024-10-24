7f30dbcbc2386d54a9f00d719b972b76cd0cfa49
Jon-1103 <jonathan.martins3@fatec.sp.gov.br>
2024-10-24 10:47:19 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ document.addEventListener('DOMContentLoaded', function () {
   // Create a map instance and set the initial view coordinates and zoom level
   var map = L.map('map').setView([-23.516858811278148, -46.83549726328389], 13);
 
+  var greenIcon = L.icon({
+    iconUrl: 'https://i.redd.it/u68122mapmx31.png',
+    iconSize:     [80, 60], // size of the icon
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
 
   // Add a tile layer to the map from OpenStreetMap
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -13,4 +20,7 @@ document.addEventListener('DOMContentLoaded', function () {
   // Create a marker with popup and add it to the map
   var marker = L.marker([-23.516858811278148, -46.83549726328389]).addTo(map);
   marker.bindPopup("Salve Salveeee").openPopup();
+
+  var marker2 = L.marker([-23.52026413037824, -46.83670586136634], {icon: greenIcon}).addTo(map);
+  marker2.bindPopup("Amada turma de DMD, eu me perdi aqui").openPopup();
 });
